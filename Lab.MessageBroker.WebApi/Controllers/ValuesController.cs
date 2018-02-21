ab7b9c18951c0ada0dc98e025aef41fb0c151cd4
Felipe Jesus <felipepiresdejesus@gmail.com>
2018-02-21 13:26:33 -0300

    Add project

---
+++ Lab.MessageBroker.WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+
+namespace Lab.MessageBroker.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get(string message)
+        {
+            var factory = new ConnectionFactory() { HostName = "192.168.99.100" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+                return new[] { $"Message sent: {message}" };
+            }
+        }
+    }
+}
